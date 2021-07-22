34dde83d08c61c58307d198ed045556dba476b7b
Teo Nogueira <teonogueira17@gmail.com>
2021-07-22 17:10:18 -0300

    Aula_03_min_1:08:15_codeCopy_and_saveAsksInForumInDataBase

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,10 +1,59 @@
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
+import { RoomCode } from '../components/RoomCode'
+import {useParams} from 'react-router-dom'
+
+
 import '../styles/room.scss'
+import { FormEvent, useState } from 'react'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+
+type RoomParams = {
+
+    id: string;
+}
 
 
 export function Room() {
+const {user} = useAuth()
+const params = useParams<RoomParams>();
+const [newQuestion, setNewQuestion] = useState('');
 
+const roomId = params.id;
+
+
+async function handleSendQuestion(event: FormEvent) {
+
+event.preventDefault();
+
+    if(newQuestion.trim() === '') {
+
+        return;
+    }
+
+    if(!user) {
+
+        throw new Error('You must be logged in')
+    }
+
+const question = {
+
+content: newQuestion,
+author: {
+name: user?.name,
+avatar: user.avatar,
+
+
+},
+
+isHighLighted: false,
+isAnswered: false,
+};
+
+await database.ref(`rooms/${roomId}/questions`).push(question)
+
+}
 return(
 
 <div id="page-room">
@@ -12,7 +61,7 @@ return(
 <header>
     <div className="content">
 <img src={logoImg} alt="LetMeAsk" />
-<div>código</div>
+<RoomCode code={roomId}/>
     </div>
 
 </header>
@@ -24,8 +73,11 @@ return(
     <span>4 perguntas</span>
 </div>
 
-<form>
-    <textarea placeholder="O que você quer perguntar?">
+<form onSubmit={handleSendQuestion}>
+    <textarea placeholder="O que você quer perguntar?"
+    onChange={event => setNewQuestion(event.target.value)}
+    value={newQuestion}
+    >
       
 
 
@@ -33,7 +85,7 @@ return(
     <div className="form-footer">
 
         <span>Para enviar uma pergunta, <button>faça seu login.</button></span>
-        <Button type="submit">Enviar Pergunta</Button>
+        <Button type="submit" disabled={!user}>Enviar Pergunta</Button>
 
     </div>
 </form>
